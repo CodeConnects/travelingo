3376b3ddbf372caf42bd484c36f289105195e84c
Scott <4674926+CodeConnects@users.noreply.github.com>
2018-04-12 14:59:33 -0400

    added gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp"),
+    watch = require("gulp-watch");
+
+gulp.task("default", function() {
+    console.log("Yeahhhh boyyyy, nice Gulp task!!!");
+});
+
+gulp.task("html", function() {
+    console.log("Here is some awesome automated html thingy");
+});
+
+gulp.task("styles", function() {
+    console.log("automated Sass or PostCSS task here");
+});
+
+gulp.task("watch", function(){
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    });
+});
